6d954e6815d1e9e21e3f8caeff363a05222d52b6
masterShake <vpgiannini@yahoo.com>
2014-06-05 14:39:38 -0700

    uploaded my files
    what I have so far

---
+++ words_to_remove.py
@@ -0,0 +1,2 @@
+bad_words = ["this", "guy", "nice", "hot", "a", "the", "im", "someone", "wow", "en", "is", "love", "are", "do", "was", "reblog", "like", "wish", "fucking", "way", "un", "dude", "have", "had", "id", "omg", "omfg", "lol", "saw", "haha", "kinda", "forever", "being", "be", "sexy", "fuck", "oh", "ok", "got", "get", "c", "see", "good", "lord", "enjoy", "sweet"]
+bad = set(bad_words)
